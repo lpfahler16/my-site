fa407c071afe69ce84e9b856bc1cd80915877b02
sean-seibel <seibel.s@northeastern.edu>
2023-01-12 22:26:10 -0500

    left carrot

--- pages/leaderboard.tsx
+++ pages/leaderboard.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { Leaderboard } from "../components/stack/FullStack";
-import { MdArrowBack } from "react-icons/md";
+import { FaChevronLeft } from "react-icons/fa";
 import Link from "next/link";
 
 export default function Stack() {
@@ -13,10 +13,10 @@ export default function Stack() {
       </Head>
 
       <main className="bg-darkest-gray min-h-screen overflow-hidden">
-        <section className="bg-darkest-gray px-10">
+        <section className="bg-darkest-gray px-10 relative">
           <div className="max-w-xl m-auto p-8">
             <Link href={"/stack"}>
-              <MdArrowBack className="absolute top-4 right-4 text-lightest-gray text-5xl" />
+              <FaChevronLeft className="absolute top-4 left-4 text-lightest-gray text-5xl" />
             </Link>
             <p className="text-2xl text-lightest-gray py-4">Leaderboard</p>
             <Leaderboard />
